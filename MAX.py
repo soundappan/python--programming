156796803225de9a33ea002581462ff48060fbc2
Divya <divyagovind23@gmail.com>
2018-07-23 11:41:18 +0530

    Create MAX.py

---
+++ MAX.py
@@ -0,0 +1,7 @@
+number = list[]
+number = input("Enter the number of elements you want:")
+print ('Enter numbers in array: ')
+for i in range(int(number)):
+    n = input("number :")
+    numberarray.append(int(n))
+print ('ARRAY: ',numberarray)
