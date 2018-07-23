68c9e3222a4ba6144029017aaa8a6c2f9a408799
Divya <divyagovind23@gmail.com>
2018-07-23 11:45:04 +0530

    Create min.py

---
+++ min.py
@@ -0,0 +1,6 @@
+number = list[]
+number = input("Enter the number of elements you want:")
+print ('Enter numbers in array: ')
+for i in range(int(number)):
+    n = input("number :")
+print ('ARRAY: ',numberarray)
