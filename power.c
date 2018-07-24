5d8a28d90cbf22431613fd07ce36796300eddaaf
Divya <divyagovind23@gmail.com>
2018-07-24 16:16:44 +0530

    Create power.c

---
+++ power.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    if(n%2==0)
+    {
+        printf("\n%d yes",n);
+    }
+    else
+    {
+        printf("\n%d No");
+        return 0;
+    }
+}
