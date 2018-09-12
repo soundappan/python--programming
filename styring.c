5994c288ebb4a3010935cc5a21c1d64f89d5ccad
Divya <divyagovind23@gmail.com>
2018-09-12 15:50:30 +0530

    Create styring.c

---
+++ styring.c
@@ -0,0 +1,11 @@
+include<stdio.h>
+int main()
+{
+char str1[50];
+char str2[50];
+printf("Enter the str1");
+scanf("%s",&str1);
+printf("Enter the str2");
+scanf("%s",&str2);
+printf("%s%s",str1+str2);
+}
