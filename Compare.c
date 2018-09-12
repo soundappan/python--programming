1eac4326a1603371f7c45068e321c7daebd26a6a
Divya <divyagovind23@gmail.com>
2018-09-12 15:37:25 +0530

    Create Compare.c

---
+++ Compare.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+char str1[50];
+char str2[50];
+scanf("%s",&str1);
+printf("Enter the str1");
+scanf("%s",&str2);
+printf("Enter the str2");
+if(str1 >str2)
+{
+    printf("%s",str1);
+}
+else
+{
+    printf("%s", str2);
+}
+}
